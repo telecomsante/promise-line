21933e5eb63fbfaa4a94fa39200d753be128fa2f
Florian Le Roux <florian.le-roux02@orange.fr>
2016-11-30 20:43:28 +0100

    add feature simultaneous exec

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 'use strict'
 
-module.exports = function () {
-  var handle = {}
-  var line = []
-  var started = false
+module.exports = function (nbMax = 1) {
+  const handle = {}
+  const line = []
+  let nbExec = 0
 
-  var process = function () {
-    var job = line.shift()
-    if (job) {
-      job.factory().then(job.resolve).catch(job.reject).then(process)
-    } else {
-      started = false
-    }
-  }
+  const process = function () {
+    if (nbExec === nbMax) return
 
-  var start = function () {
-    if (started) { return }
-    started = true
-    process()
+    const job = line.shift()
+    if (!job) return
+
+    nbExec++
+    job.factory().then(job.resolve).catch(job.reject)
+      .then(() => {
+        nbExec--
+        process()
+      })
   }
 
   handle.push = function (promiseFactory) {
-    var job = { factory: promiseFactory }
+    const job = { factory: promiseFactory }
     line.push(job)
     return new Promise(function (resolve, reject) {
       job.resolve = resolve
       job.reject = reject
-      start()
+      process()
     })
   }
 
